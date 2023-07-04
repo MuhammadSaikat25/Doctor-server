d7a59a5eb9d2c645d8becf9ca663bfe59c9262a0
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-07-04 21:42:41 +0600

    update user role

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt=require('jsonwebtoken')
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
@@ -9,7 +11,6 @@ app.get('/', (req, res) => {
   res.send('Doctor on the way..')
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.ctx1etf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,8 +31,8 @@ async function run() {
     const Doctor = client.db('TEETH-DOCTOR').collection('doctor')
     const services = client.db('TEETH-DOCTOR').collection('services')
     const reviews = client.db('TEETH-DOCTOR').collection('reviews')
-    const appointment=client.db('TEETH-DOCTOR').collection('appointment')
-    const user=client.db('TEETH-DOCTOR').collection('user')
+    const appointment = client.db('TEETH-DOCTOR').collection('appointment')
+    const user = client.db('TEETH-DOCTOR').collection('user')
     // Send a ping to confirm a successful connection
 
     // getting doctor data
@@ -40,27 +41,50 @@ async function run() {
       res.send(result)
     })
     // getting services data
-    app.get('/services',async(req,res)=>{
-      const result=await services.find().toArray()
+    app.get('/services', async (req, res) => {
+      const result = await services.find().toArray()
       res.send(result)
     })
-
+    // create jwt 
+    app.post('/jwt',async(req,res)=>{
+      const user=req.body
+      const token= jwt.sign(user,process.env.VITE_JWT,{expiresIn:'1d'})
+      res.send(token)
+    })
     //get reviews data
-    app.get('/reviews',async(req,res)=>{
-      const result=await reviews.find().toArray()
+    app.get('/reviews', async (req, res) => {
+      const result = await reviews.find().toArray()
       res.send(result)
     })
 
     // added appointment 
-    app.post('/Patients',async(req,res)=>{
-      const data=req.body
-      const result=await appointment.insertOne(data)
+    app.post('/Patients', async (req, res) => {
+      const data = req.body
+      const result = await appointment.insertOne(data)
       res.send(result)
     })
 
-    app.post('/user',async(req,res)=>{
-      const data=req.body
-      const result=await user.insertOne(data)
+    app.post('/user', async (req, res) => {
+      const data = req.body
+      const result = await user.insertOne(data)
+      res.send(result)
+    })
+    //get all user
+    app.get('/users', async (req, res) => {
+      const result = await user.find().toArray()
+      res.send(result)
+    })
+
+    // make user to admin
+    app.patch('/user/admin/:id',async(req,res)=>{
+      const id=req.params.id 
+      const filter={_id:new ObjectId(id)}
+      const update={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result =await user.updateOne(filter,update)
       res.send(result)
     })
 
@@ -72,8 +96,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
 
 app.listen(port)
 
